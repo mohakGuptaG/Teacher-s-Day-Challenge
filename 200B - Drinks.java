03e7038c517ed3ea81249b2ae32dffb926f4cf11
mohakGuptaG <campionite0@gmail.com>
2025-08-27 21:11:25 +0530

    Day-27

---
+++ 200B - Drinks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Drinks{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double sum =0;
+
+        for(int i=0; i<n; i++){
+            sum = sum + sc.nextInt();
+        }
+
+        System.out.println(sum/n);
+    }
+}
